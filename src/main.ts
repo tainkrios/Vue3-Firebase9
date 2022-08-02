ee4c2a1d5ebf3e5cb45e4d0e02e2169e72840d66
Ivan Zhdanov <tain.krios@gmail.com>
2022-08-02 13:18:03 +0200

    waiting auth

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,13 @@
-import { createApp } from 'vue'
+import { ComponentOptionsBase, ComponentPublicInstance, createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import { auth } from '@/firebase/config'
+import { onAuthStateChanged } from '@firebase/auth'
 
-createApp(App).use(router).mount('#app')
+let app: ComponentPublicInstance<{}, {}, {}, {}, {}, {}, {}, {}, false, ComponentOptionsBase<any, any, any, any, any, any, any, any, any, {}>>
+
+onAuthStateChanged(auth, () => {
+  if (!app) {
+    app = createApp(App).use(router).mount('#app')
+  }
+})
